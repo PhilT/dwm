2c01794216d02dbc1c97c849affce29e515f4e5d
Phil Thompson <34678+PhilT@users.noreply.github.com>
2023-10-26 15:38:45 +0100

    Add column patch

--- config.h
+++ config.h
@@ -43,7 +43,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-//	{ "|||",      col },
+	{ "|||",      col },
 //{ "###",      gaplessgrid },
 };
 
